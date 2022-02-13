0365b0ab4417782db2b78c66b35192d558695949
Rene <king-sora@hotmail.de>
2022-02-13 17:00:01 +0100

    improve docu

--- src/types.ts
+++ src/types.ts
@@ -8,22 +8,24 @@ import {
   ReactElement,
 } from 'react';
 
-export type ComponentPropsDefault = Record<string, unknown>;
-export type InputComponentProps<C extends InputComponent> = C extends JSXElementConstructor<any> | keyof JSX.IntrinsicElements
-  ? ComponentProps<C>
-  : ComponentPropsDefault;
-
 /**
- * "wrap" the "component" always with the "as" component
- * "leave" the "component" alone and do nothing if the transformation wasn't sucessful
+ * The strategy for how to deal with the transformation of the component if a invalid root node was found.
+ * wrap: wrap the "component" always with the "as" component
+ * leave: leave the "component" alone and do nothing if the transformation wasn't sucessful
  */
 export type Strategy = 'leave' | 'wrap';
 
+/**
+ * Overwrite props function used to combine or overwrite props.
+ */
 export type OverwriteProps<CompProps = ComponentPropsDefault, AsProps = ComponentPropsDefault> = (
   compProps: CompProps,
   asProps: AsProps
 ) => CompProps & AsProps;
 
+/**
+ * Options used to customize the component transformation process.
+ */
 export interface Options<CompProps = ComponentPropsDefault, AsProps = ComponentPropsDefault> {
   // The strategy how to continue in case the process would fail.
   strategy: Strategy;
@@ -54,3 +56,15 @@ export type ValidComponentType = CallableComponentType | string;
  * ComponentTypes can also be invalid types like "ReactElement" thus the ts-ignore in some places
  */
 export type ComponentType = ReactElement | ValidComponentType;
+
+/**
+ * Helper type for unknown or not inferable component props.
+ */
+export type ComponentPropsDefault = Record<string, unknown>;
+
+/**
+ * Helper type to infer component props from the JSXElementConstructor or JSX.IntrinsicElements.
+ */
+export type InputComponentProps<C extends InputComponent> = C extends JSXElementConstructor<any> | keyof JSX.IntrinsicElements
+  ? ComponentProps<C>
+  : ComponentPropsDefault;
