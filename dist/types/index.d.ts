2af52f662da72073acef930ea29c09b0caa1bc3c
Rene <king-sora@hotmail.de>
2022-02-13 16:17:16 +0100

    remove dist from repo

--- dist/types/index.d.ts
+++
@@ -1,4 +0,0 @@
-import As, { transform, AsProps } from './As';
-import { Options, Strategy, OverwriteProps } from './types';
-export { As, transform, AsProps, Options, Strategy, OverwriteProps };
-export default As;
